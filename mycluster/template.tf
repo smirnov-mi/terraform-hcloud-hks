6e55758e3b109c8b970f227c543fa5a0d6bda646
Mikhail Smirnov <smirnov.mi@gmail.com>
2024-06-07 10:45:55 +0200

    empty sshkey

--- mycluster/template.tf
+++ mycluster/template.tf
@@ -19,7 +19,7 @@ module "hks" {
 
   # Map of ssh keys that can be used to connect to the nodes.
   ssh_public_keys = {
-    my-ssh-key = "..."
+    my-ssh-key = ""
   }
 
   hcloud_token = local.hcloud_token
