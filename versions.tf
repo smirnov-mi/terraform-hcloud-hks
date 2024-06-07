f929940ddff92adc44005654b03f7dad90fa0d28
Mikhail Smirnov <smirnov.mi@gmail.com>
2024-06-07 13:35:37 +0200

    versions updated

--- versions.tf
+++ versions.tf
@@ -1,20 +1,20 @@
 terraform {
-  required_version = ">= 1.3.0"
+  required_version = ">= 1.8.0"
 
   required_providers {
     hcloud = {
       source  = "hetznercloud/hcloud"
-      version = "1.36.2"
+      version = "1.47.0"
     }
 
     tls = {
       source  = "hashicorp/tls"
-      version = "4.0.4"
+      version = "4.0.5"
     }
 
     ssh = {
       source  = "loafoe/ssh"
-      version = "2.6.0"
+      version = "2.7.0"
     }
   }
 }
